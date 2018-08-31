637cd4d4f81128f2d1d4624b164d43b745a425e7
MariesNavarro <mariesnavarro@gmail.com>
2018-08-31 15:10:29 -0500

    maqueta demo

--- maq/ui/js/front.js
+++ maq/ui/js/front.js
@@ -2,7 +2,7 @@
 window.console.log("%cCoded by Oet Capital", "color:#fff;  font-size: 10px; background:#000; padding:20px;");
 function _(el){return document.querySelector(el); }
 function __(el){return document.querySelectorAll(el); }
-function dB(){
+function detectBrowser(){
   var cB = false;
   if(bowser.mobile || bowser.tablet || /SymbianOS/.test(window.navigator.userAgent)) checkMobile = true;
   var r = {
@@ -12,13 +12,40 @@ function dB(){
     hD: "1080"
   }
   if(cB){
-    setRatio("m");
+    // setRatio("m");
   } else {
-    setRatio("d");
+    // setRatio("d");
   }
 }
-function gC(){
+function generateCoupon(){
   var tx = _('#stateText').innerHTML = "Cargando Cupón...";
   var carrusel = _('#carrusel').style.display = "none";
+  var generando = _("#generandocupon").style.display = "block";
+  loadingCoupon();
+}
+
+function loadingCoupon(){
+  var generando = _("#generandocupon");
+  var n = 0;
+  var counter = _("#counter>p");
+  var interval = setInterval(function(){
+    n++;
+    counter.innerHTML = n;
+    if(n===100) {clearInterval(interval);
+      generando.style.display = "none";
+      displayCoupon()
+    }
+  });
+}
+
+
+function displayCoupon(){
   var cupon = _("#cupon").style.display = "block";
+  var tx = _('#stateText').innerHTML = "Cupón Listo";
+}
+
+function savedCoupon(){
+  var cupon = _("#cupon").style.display = "none";
+  var guardado = _("#guardado").style.display = "block";
+  var tx = _('#stateText').innerHTML = "Cupón Guardado Exitosamente";
 }
