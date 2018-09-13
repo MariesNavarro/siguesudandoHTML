16c7557d385e5d6f6507497271c17a7647377154
MariesNavarro <mariesnavarro@gmail.com>
2018-09-13 13:47:52 -0500

    comment countdown min

--- int/index.php
+++ int/index.php
@@ -14,7 +14,7 @@ Sigue sudando v2
     <meta name="viewport" content="width=device-width, initial-scale=1.0000, minimum-scale=1.0000, maximum-scale=1.0000, user-scalable=no">
     <title>Sigue Sudando | Gatorade ®</title>
     <link rel="stylesheet" href="ui/css/master.min.css">
-    <!-- <link rel="stylesheet" href="countdown/count.css"> -->
+    <!-- <link rel="stylesheet" href="countdown/count.min.css"> -->
     <link rel="prefetch" href="ui/img/logotipo-gatorade.svg">
     <link rel="canonical" href="www.siguesudando.com">
     <link rel="apple-touch-icon" sizes="180x180" href="ui/fav/apple-touch-icon.png">
