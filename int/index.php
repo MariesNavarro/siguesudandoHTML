900d2ec19ffc80657c53e3b2399222dcd732ec0e
MariesNavarro <mariesnavarro@gmail.com>
2018-09-13 16:29:17 -0500

    optimize (Block css)

--- int/index.php
+++ int/index.php
@@ -13,8 +13,9 @@ Sigue sudando v2
     <meta name="google" content="notranslate">
     <meta name="viewport" content="width=device-width, initial-scale=1.0000, minimum-scale=1.0000, maximum-scale=1.0000, user-scalable=no">
     <title>Sigue Sudando | Gatorade ®</title>
-    <link rel="stylesheet" href="ui/css/master.min.css">
-    <!-- <link rel="stylesheet" href="countdown/count.min.css"> -->
+    <style>
+      * a,body{color:#000}#index,#loading{background:#ff4d00}a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}ol,ul{list-style:none}#s{display:none}*{margin:0;padding:0;box-sizing:border-box}* a{cursor:pointer;text-decoration:none}* i{font-style:italic}* p{font-size:1rem}.trans3{-webkit-transition:all .3s ease-out;-moz-transition:all .3s ease-out;-ms-transition:all .3s ease-out;-o-transition:all .3s ease-out;transition:all .3s ease-out}.trans7{-webkit-transition:all .7s ease-out;-moz-transition:all .7s ease-out;-ms-transition:all .7s ease-out;-o-transition:all .7s ease-out;transition:all .7s ease-out}.flexDisplay{display:-webkit-box;display:-ms-flexbox;display:-webkit-flex;display:flex}.dislplayNone{display:none}.standarWidth{width:100vw;min-width:250px}#loading>div,#loading>div>img{max-width:280px;min-width:110px}#bases{background:#fff}body{position:relative}#loading{position:fixed;top:0;left:0;height:100vh;-webkit-flex-direction:column;-moz-flex-direction:column;-ms-flex-direction:column;-o-flex-direction:column;flex-direction:column;-webkit-align-items:center;-moz-align-items:center;-ms-align-items:center;-o-align-items:center;align-items:center;-webkit-justify-content:center;-moz-justify-content:center;-ms-justify-content:center;-o-justify-content:center;justify-content:center;z-index:5000;opacity:1}#loading>div{text-align:center;width:80%;height:200px}#loading>div>img{width:25%;height:100px;opacity:0;-webkit-animation:fadeIn .7s ease-in .7s forwards;-moz-animation:fadeIn .7s ease-in .7s forwards;-ms-animation:fadeIn .7s ease-in .7s forwards;-o-animation:fadeIn .7s ease-in .7s forwards;animation:fadeIn .7s ease-in .7s forwards}#loading>div>div{margin:0 auto;width:250px;height:30px;-webkit-justify-content:center;-moz-justify-content:center;-ms-justify-content:center;-o-justify-content:center;justify-content:center;-webkit-align-items:flex-end;-moz-align-items:flex-end;-ms-align-items:flex-end;-o-align-items:flex-end;align-items:flex-end;opacity:0;-webkit-animation:fadeIn .7s ease-in 1.2s forwards;-moz-animation:fadeIn .7s ease-in 1.2s forwards;-ms-animation:fadeIn .7s ease-in 1.2s forwards;-o-animation:fadeIn .7s ease-in 1.2s forwards;animation:fadeIn .7s ease-in 1.2s forwards}#loading>div>div>p{text-transform:uppercase;font-size:1.1rem;color:#fff;margin-right:3px}#loading>div>div>span{display:block;width:7px;height:7px;background:#fff;border-radius:50%;margin:0 2px 8px;-webkit-animation:scaleDot 1.5s infinite;-moz-animation:scaleDot 1.5s infinite;-ms-animation:scaleDot 1.5s infinite;-o-animation:scaleDot 1.5s infinite;animation:scaleDot 1.5s infinite}#loading>div>div>span:nth-child(2){-webkit-animation-delay:1s;-moz-animation-delay:1s;-ms-animation-delay:1s;-o-animation-delay:1s;animation-delay:1s}#loading>div>div>span:nth-child(3){-webkit-animation-delay:1.5s;-moz-animation-delay:1.5s;-ms-animation-delay:1.5s;-o-animation-delay:1.5s;animation-delay:1.5s}#loading>div>div>span:nth-child(4){-webkit-animation-delay:2s;-moz-animation-delay:2s;-ms-animation-delay:2s;-o-animation-delay:2s;animation-delay:2s}@-webkit-keyframes fadeIn{from{opacity:0}to{opacity:1}}@-moz-keyframes fadeIn{from{opacity:0}to{opacity:1}}@-ms-keyframes fadeIn{from{opacity:0}to{opacity:1}}@-o-keyframes fadeIn{from{opacity:0}to{opacity:1}}@keyframes fadeIn{from{opacity:0}to{opacity:1}}@-webkit-keyframes scaleDot{0%,100%{opacity:1;-webkit-transform:scale(1)}50%{opacity:0;-webkit-transform:scale(0)}}@-moz-keyframes scaleDot{0%,100%{opacity:1;-moz-transform:scale(1)}50%{opacity:0;-moz-transform:scale(0)}}@-ms-keyframes scaleDot{0%,100%{opacity:1;-ms-transform:scale(1)}50%{opacity:0;-ms-transform:scale(0)}}@-o-keyframes scaleDot{0%,100%{opacity:1;-o-transform:scale(1)}50%{opacity:0;-o-transform:scale(0)}}@keyframes scaleDot{0%,100%{opacity:1;transform:scale(1)}50%{opacity:0;transform:scale(0)}}
+    </style>
     <link rel="prefetch" href="ui/img/logotipo-gatorade.svg">
     <link rel="canonical" href="www.siguesudando.com">
     <link rel="apple-touch-icon" sizes="180x180" href="ui/fav/apple-touch-icon.png">
@@ -31,14 +32,13 @@ Sigue sudando v2
     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
     <script async src="backend/js/fingerprint2.min.js"></script>
     <script src="backend/js/app.min.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="analytics/gtag.min.js"></script>
     <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-125324668-1');
-  </script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-125324668-1');
+    </script>
   </head>
   <body id="index" class="standarWidth">
     <!-- LOADING -->
@@ -46,7 +46,7 @@ Sigue sudando v2
       <div>
         <img src="ui/img/logotipo-gatorade.svg" alt="Logotipo Gatorade ®" width="280" height="280">
         <div class="flexDisplay">
-          <p>Cargando</p>
+          <p></p>
           <span></span>
           <span></span>
           <span></span>
@@ -167,30 +167,34 @@ Sigue sudando v2
     <script src="ui/js/bowser.min.js" charset="utf-8"></script>
     <script src="ui/js/front.min.js" charset="utf-8"></script>
     <script>
-      window.onload = function(){ valido(); }
+      function cssBlock(){ valido(); }
+      window.onload = function(){
+        var head = document.getElementsByTagName("HEAD")[0];
+        var link = document.createElement("LINK");
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("href", "ui/css/master.min.css");
+        link.setAttribute("onload", "cssBlock()");
+        head.appendChild(link);
+      }
     </script>
   </body>
 </html>
 <?php
   ob_end_flush();
  function comprimir_pagina($buffer) {
-
     $search = array(
-        '/\>[^\S ]+/s',     // elimina espacios en blanco después de las etiquetas, excepto el espacio
-        '/[^\S ]+\</s',     // elimina en blanco antes de las etiquetas, excepto el espacio
-        '/(\s)+/s',         // Acortar múltiples secuencias de espacios en blanco.
-        '/<!--(.|\s)*?-->/' // Borrar comentarios html
+        '/\>[^\S ]+/s',
+        '/[^\S ]+\</s',
+        '/(\s)+/s',
+        '/<!--(.|\s)*?-->/'
     );
-
     $replace = array(
         '>',
         '<',
         '\\1',
         ''
     );
-
     $buffer = preg_replace($search, $replace, $buffer);
-
     return $buffer;
    }
 ?>
